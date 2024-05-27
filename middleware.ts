f652576819c9260af140015fb5aadb0f69d820f2
Dustin Bowers <dustin.uncc@gmail.com>
2024-05-26 23:18:40 -0400

    Use Uploadthing. Add file upload component and routes. Prepare for server creation through api. (2:09)

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,39 @@
-import { createRouteMatcher, clerkMiddleware } from "@clerk/nextjs/server";
+// import { createRouteMatcher, clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-    '/' // Add any additional routes here
-]);// Update clerkMiddleware to manually protect routes
+// const isProtectedRoute = createRouteMatcher([
+//     '/' // Add any additional routes here
+// ]);// Update clerkMiddleware to manually protect routes
 
 
-export default clerkMiddleware((auth, req) => {
-    if (isProtectedRoute(req)) {
-        auth().protect(); // Protect the route if it matches the defined criteria
+// export default clerkMiddleware((auth, req) => {
+//     if (isProtectedRoute(req)) {
+//         auth().protect(); // Protect the route if it matches the defined criteria
+//     }
+// });
+
+// export const config = {
+//     matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+import { createRouteMatcher, clerkMiddleware } from '@clerk/nextjs/server';
+
+const isPublicRoute = createRouteMatcher([
+    '/sign-in',
+    '/sign-up',
+    '/api/uploadthing',
+]);
+
+export default clerkMiddleware((auth, request) => {
+    if (!isPublicRoute(request)) {
+        auth().protect();
     }
-});
+}, { debug: true });
 
 export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+    // The following matcher runs middleware on all routes
+    // except static assets.
+    matcher: [
+        '/((?!.*\\..*|_next).*)',
+        '/',
+        '/(api|trpc)(.*)'
+    ],
 };
