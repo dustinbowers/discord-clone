8645ccca81ea09f802b243a69adf6037d2ff1fd1
Dustin Bowers <dustin.uncc@gmail.com>
2024-05-29 22:39:16 -0400

    Implement edit message feature (10:29)

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { Member, Message, Profile } from "@prisma/client";
+import { format } from "date-fns";
 
 import { ChatWelcome } from "./chat-welcome";
 import { useChatQuery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
+import { ChatItem } from "./chat-item";
+
+const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
 type MessageWithMemberWithProfile = Message & {
     member: Member & {
@@ -84,7 +88,19 @@ export const ChatMessages = ({
                     <Fragment key={i}>
                         {group.items.map((message: MessageWithMemberWithProfile) => (
                             <div key={message.id}>
-                                {message.content}
+                                <ChatItem
+                                    key={message.id}
+                                    id={message.id}
+                                    currentMember={member}
+                                    member={message.member}
+                                    content={message.content}
+                                    fileUrl={message.fileUrl}
+                                    deleted={message.deleted}
+                                    timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                                    isUpdated={message.updatedAt !== message.createdAt}
+                                    socketUrl={socketUrl}
+                                    socketQuery={socketQuery}
+                                />
                             </div>
                         ))}
                     </Fragment>
